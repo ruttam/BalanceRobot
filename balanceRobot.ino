8eaca499d037eb2a0a23ccd7d426a215c32527dc
Henrikas Amosejevas <Henrikas.Amosejevas@devbridge.com>
2016-12-08 12:53:27 +0200

    Use moveWheel. Update structure and parameters.

--- balanceRobot.ino
+++ balanceRobot.ino
@@ -10,6 +10,10 @@ const int rightWheelPin1 = 8;
 const int rightWheelPin2 = 9;
 const int rightWheelVelocityPin3 = 10;
 
+int leftWheel[3] = {leftWheelPin1, leftWheelPin2, leftWheelVelocityPin3};
+int rightWheel[3] = {rightWheelPin1, rightWheelPin2, rightWheelVelocityPin3};
+
+
 const int FORWARD = 1;
 const int REVERSE = 0;
 
@@ -21,6 +25,7 @@ int count;
 int bluetoothInput[3] = {0, 0, 0};
 float x = 0;
 char floatstr[15];
+int printCount;
 
 void setup(void) {
   Serial.begin(9600);
@@ -36,104 +41,73 @@ void setup(void) {
 }
 
 void loop(void) {
-  readAccelerometerData();
-
-    //Reading input from serial
+  float angle = readAccelerometerData();
+  printCount++;
+  if(printCount > 200) {
+    Serial.println(angle);
+    printCount = 0; 
+  }
+  
+  //Reading input from serial
   if (Serial.available() > 0) {
     bluetoothInput[readCount] = Serial.read();
 
     if (readCount == 2)  {
       Serial.println("Conneted via Bluetooth: " + String(bluetoothInput[0]));
       readCount = 0;
-    } else {
-      readCount++;
-    } 
-  }
+      } else {
+        readCount++;
+      } 
+    }
 
     //If input from serial "Go straight"
-  if (bluetoothInput[1] == 97) {
+    if (bluetoothInput[1] == 97) {
     //TODO: Implementation
   }
 
-    //If falling to the back - go to the front
-  if (x < -1 && x > -4) {
-      //moveWheel(
-    digitalWrite(leftWheelPin1, HIGH);
-    digitalWrite(leftWheelPin2, LOW);
-    digitalWrite(leftWheelVelocityPin3, 20);
-    digitalWrite(rightWheelPin1, LOW);
-    digitalWrite(rightWheelPin2, HIGH);
-    digitalWrite(rightWheelVelocityPin3, 20);
-    //If fell to the back - do not move
-  } else if(x < -5) {
-    digitalWrite(leftWheelPin1, LOW);
-    digitalWrite(leftWheelPin2, LOW);
-    digitalWrite(leftWheelVelocityPin3, 0);
-    digitalWrite(rightWheelPin1, LOW);
-    digitalWrite(rightWheelPin2, LOW);
-    digitalWrite(rightWheelVelocityPin3, 0);
-    //If falling to the front - go back
-  } else if (x > 1 && x < 4) {
-    digitalWrite(leftWheelPin1, LOW);
-    digitalWrite(leftWheelPin2, HIGH);
-    digitalWrite(leftWheelVelocityPin3, 20);
-    digitalWrite(rightWheelPin1, HIGH);
-    digitalWrite(rightWheelPin2, LOW);
-    digitalWrite(rightWheelVelocityPin3, 20);
-    //If fell to the front - do not move
-  } else if(x > 5) {
-    digitalWrite(leftWheelPin1, LOW);
-    digitalWrite(leftWheelPin2, LOW);
-    digitalWrite(leftWheelVelocityPin3, 0);
-    digitalWrite(rightWheelPin1, LOW);
-    digitalWrite(rightWheelPin2, LOW);
-    digitalWrite(rightWheelVelocityPin3, 0);
-    //If standing straight - do not move
-  } else if(x > -2 && x <2){
-    digitalWrite(leftWheelPin1, LOW);
-    digitalWrite(leftWheelPin2, LOW);
-    digitalWrite(leftWheelVelocityPin3, 0);
-    digitalWrite(rightWheelPin1, LOW);
-    digitalWrite(rightWheelPin2, LOW);
-    digitalWrite(rightWheelVelocityPin3, 0);
-  }
-}
+  if (angle > 5) {
+    moveWheel(leftWheel, FORWARD, 75);
+    moveWheel(rightWheel, REVERSE, 75);
+    } else if (angle < -5) {
+      moveWheel(leftWheel, REVERSE, 75);
+      moveWheel(rightWheel, FORWARD, 75);
+      } else {
+        moveWheel(leftWheel, FORWARD, 0);
+        moveWheel(rightWheel, REVERSE, 0);
+      }
+    }
 
-void readAccelerometerData(void) {
-  sensors_event_t accelEvent;
-  accel.getEvent(&accelEvent);
+    float readAccelerometerData() {
+      sensors_event_t accelEvent;
+      accel.getEvent(&accelEvent);
     //Read every 200th data from accelerometer sensor
-  if(count == 200) {
-    x = accelEvent.acceleration.x;
-    printFloat("Accelerometer x value:", normalizeAngle(x));
-    count = 0;
-  } else {
-    count++;
-  }
+    if(count == 20) {
+      x = accelEvent.acceleration.x;
+//    printFloat("Accelerometer x value:", normalizeAngle(x));
+count = 0;
+} else {
+  count++;
 }
 
-void moveWheel(int wheel, int direction, int throttle) {
-  if(throttle > 0){
-    digitalWrite(wheel, direction == FORWARD ? LOW : HIGH);
-    digitalWrite(wheel, direction == FORWARD ? HIGH : LOW);
-    digitalWrite(wheel, throttle);
-  } else {
-    digitalWrite(wheel, LOW);
-    digitalWrite(wheel, LOW);
-    digitalWrite(wheel, 0);
-  }
+return normalizeAngle(x);
+}
+
+void moveWheel(int wheel[3], int direction, int throttle) {
+  digitalWrite(wheel[0], direction == FORWARD ? LOW : HIGH);
+  digitalWrite(wheel[1], direction == FORWARD ? HIGH : LOW);
+  digitalWrite(wheel[2], throttle);
 }
 
 float normalizeAngle(float angle) {
-  //We multiply reading by 9 to get normalized value.
   return angle * 9;
 }
 
 void printFloat(char *label, float value) {
-    //Since printf() does not include float or double numbers printing
+  //Since printf() does not include float or double numbers printing
     //we need to convert float to string with dtostrf() function
     //http://www.hobbytronics.co.uk/arduino-float-vars
-  dtostrf(x, 7, 3, floatstr);
+    dtostrf(x, 7, 3, floatstr);
     //TODO: check if we need to extract value from pointer or not. (On the spot)
-  Serial.printf("%s %s \n", &label, floatstr);
-}
+    Serial.print(label);
+    Serial.println(value);
+  }
